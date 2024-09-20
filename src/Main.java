bea6fb7f124220368809fa85e673033451af6090
drkast <imdawar090@gmail.com>
2024-09-21 04:55:37 +0500

    Usage of array

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int [] numbers={1,2,3,4,5};
+        System.out.println(numbers.length);
+        System.out.println(Arrays.toString(numbers));
     }
 }
